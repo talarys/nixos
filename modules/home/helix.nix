c22deea038022b82b1483c324a479ed14cf4b0e7
talarys <d.simon.ssx0@gmail.com>
2025-05-27 20:36:39 +0200

    Add language configuration for Nix in Helix settings

--- modules/home/helix.nix
+++ modules/home/helix.nix
@@ -5,5 +5,12 @@
     settings = {
       theme = "catppuccin_mocha";
     };
+    languages = {
+      nix = {
+        name = "nix";
+        formatter = "alejandra";
+        auto-format = true;
+      };
+    };
   };
 }
