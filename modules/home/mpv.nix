191143640345efad9c3057de2e5138d75f5f5346
talarys <d.simon.ssx0@gmail.com>
2025-06-07 22:14:53 +0200

    moved mpv config to its own repo

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -1,9 +1,13 @@
-{
+{pkgs, ...}: {
   programs.mpv = {
     enable = true;
   };
   home.file.mpv = {
-    source = ../../configs/mpv;
+    source = pkgs.fetchgit {
+      url = "https://github.com/talarys/mpv_config.git";
+      rev = "eng";
+      sha256 = "066cfe27c72422ec42f7e34b0847c308e49fcda8";
+    };
     target = ".config/mpv";
   };
 }
