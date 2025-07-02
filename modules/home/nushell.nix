b52473575bbae9f9aa2c45845796290a9570558b
talarys <d.simon.ssx0@gmail.com>
2025-07-02 20:12:21 +0200

    Add shell aliases to Nushell configuration

--- modules/home/nushell.nix
+++ modules/home/nushell.nix
@@ -6,6 +6,14 @@
         $env.config.show_banner = false
       '';
       shellAliases = {
+        cd = "z";
+        grep = "rg";
+        cat = "bat";
+        cp = "cp -i";
+        mv = "mv -i";
+        rm = "rm -i";
+        npm = "bun";
+        npx = "bunx";
       };
     };
   };
