38cd43b60002204cf8092d0c80a1f2c59a05e82e
talarys <d.simon.ssx0@gmail.com>
2025-07-02 18:06:52 +0200

    Add configuration for 'pay-respects' program with Fish and Nushell integration

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -69,6 +69,12 @@
       enableFishIntegration = true;
       enableNushellIntegration = true;
     };
+
+    pay-respects = {
+      enable = true;
+      enableFishIntegration = true;
+      enableNushellIntegration = true;
+    };
   };
 
   home.packages = with pkgs; [
