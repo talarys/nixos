14acf75ea5af5f84ea3d0cb8fb4f1fe1da8790fb
talarys <d.simon.ssx0@gmail.com>
2025-07-07 17:14:43 +0200

    Remove flox configuration from flake.nix

--- flake.nix
+++ flake.nix
@@ -29,11 +29,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    flox = {
-      url = "github:flox/flox";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     nixvim.url = "github:talarys/nvix";
   };
 
