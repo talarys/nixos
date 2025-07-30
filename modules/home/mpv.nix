1e3cfa523477ae879ce202e53b7a766860a6cd7d
talarys <d.simon.ssx0@gmail.com>
2025-07-30 23:25:48 +0200

    Fix MPV config repository URL in home configuration

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -4,9 +4,8 @@
   };
   home.file.mpv = {
     source = pkgs.fetchgit {
-      url = "https://github.com/talarys/mpv_config.git";
+      url = "https://github.com/talarys/mpv-config.git";
       rev = "eng";
-      sha256 = "066cfe27c72422ec42f7e34b0847c308e49fcda8";
     };
     target = ".config/mpv";
   };
