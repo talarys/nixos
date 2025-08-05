7d13dd543193bf6461a770ee9bd74be606c4069a
talarys <d.simon.ssx0@gmail.com>
2025-08-05 20:13:46 +0200

    Refactor default.nix to improve directory and file filtering logic

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,18 +1,37 @@
 let
-  # Read the directory and filter for .nix files
+  # Read the directory contents
+  dirContents = builtins.readDir ./.;
+
+  # Get all .nix files (regular files ending in .nix)
   nixFiles =
     builtins.filter
-    (name: builtins.match ".*\\.nix" name != null)
-    (builtins.attrNames (builtins.readDir ./.));
+    (name: dirContents.${name} == "regular" && builtins.match ".*\\.nix" name != null)
+    (builtins.attrNames dirContents);
 
-  # Create an attribute set from the filenames
+  # Get all directories that contain a default.nix
+  validDirs =
+    builtins.filter
+    (
+      name:
+        dirContents.${name}
+        == "directory"
+        && builtins.pathExists (./. + "/${name}/default.nix")
+    )
+    (builtins.attrNames dirContents);
+
+  # Combine both .nix files and valid directories
+  allEntries = nixFiles ++ validDirs;
+
+  # Create an attribute set from the entries
   imports =
     builtins.listToAttrs
     (builtins.map
       (name: {
+        # Remove .nix extension if present (for files), keep directory names as-is
         name = builtins.replaceStrings [".nix"] [""] name;
+        # Import works for both files and directories (with default.nix)
         value = import (./. + "/${name}");
       })
-      nixFiles);
+      allEntries);
 in
   imports
