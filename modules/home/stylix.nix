27fc4f00fe54fbe186be0c18dbfbca058e566c52
talarys <d.simon.ssx0@gmail.com>
2025-08-11 19:44:55 +0200

    Update stylix.nix to enable additional targets and set platform for qt

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -3,4 +3,7 @@
   stylix.targets.ghostty.enable = false;
   stylix.targets.mpv.enable = false;
   stylix.targets.helix.enable = false;
+  stylix.targets.firefox.profileNames = ["default"];
+  stylix.targets.zen-browser.profileNames = ["default"];
+  stylix.targets.qt.platform = "qtct";
 }
