8e3da7a27dfb7e311905eba4e0b64dc940772440
talarys <d.simon.ssx0@gmail.com>
2025-08-12 18:30:02 +0200

    Add configuration for ghostty and kitty programs with shared settings

---
+++ home/kitty.nix
@@ -0,0 +1,14 @@
+{
+  programs = {
+    kitty = {
+      enable = true;
+      shellIntegration.enableFishIntegration = true;
+      settings = {
+        font-family = "JetBrains Mono Nerd Font";
+        font_size = 12;
+        background-opacity = 0.9;
+        theme = "catppuccin-mocha";
+      };
+    };
+  };
+}
