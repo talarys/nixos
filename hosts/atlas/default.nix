cf6f002ea6de5c09f210b8f6ea3f58c0bf38f670
talarys <d.simon.ssx0@gmail.com>
2025-08-24 21:14:12 +0200

    Add authorized SSH keys for root and user 'talarys' in default.nix

--- hosts/atlas/default.nix
+++ hosts/atlas/default.nix
@@ -14,5 +14,13 @@
     };
   };
 
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAUAzIuK7sfr9TTOyOmWM3nm7kWZLJt0H08VLKmwlG+e d.simon.ssx0@gmail.com"
+  ];
+
+  users.users.talarys.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAUAzIuK7sfr9TTOyOmWM3nm7kWZLJt0H08VLKmwlG+e d.simon.ssx0@gmail.com"
+  ];
+
   networking.hostName = "atlas";
 }
