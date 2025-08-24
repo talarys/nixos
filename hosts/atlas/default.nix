a0f7843a386c5f8070b72b2f6b61b008733c8718
talarys <d.simon.ssx0@gmail.com>
2025-08-24 23:09:24 +0200

    added hyperv module

--- hosts/atlas/default.nix
+++ hosts/atlas/default.nix
@@ -15,6 +15,8 @@
     };
   };
 
+  virtualisation.hypervGuest.enable = true;
+
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAUAzIuK7sfr9TTOyOmWM3nm7kWZLJt0H08VLKmwlG+e d.simon.ssx0@gmail.com"
   ];
