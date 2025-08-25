293f645af182d404c75c805cef8d47fc82b44c68
talarys <d.simon.ssx0@gmail.com>
2025-08-25 22:12:02 +0200

    Remove yazi configuration file and update yazi Nix configuration to include flavors and plugins for enhanced functionality. Added support for rich previews for various file types and set the default theme flavor to catppuccin-mocha.

--- home/shell/yazi.nix
+++ home/shell/yazi.nix
@@ -1,14 +1,76 @@
-{self, ...}: {
-  home.file.yazi = {
-    source = "${self}/config/yazi";
-    target = ".config/yazi";
+{pkgs, ...}: let
+  flavors = pkgs.fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "flavors";
+    rev = "main";
+    sha256 = "sha256-xMaZoWSetXHg772cwqBTEcdXdnGYQVaiUDcjKk9r53w=";
   };
+in {
+  home.packages = with pkgs; [
+    yazi
+    rich-cli
+    glow
+  ];
+
   programs = {
     yazi = {
-      #TODO yazi.flavours
       enable = true;
       enableFishIntegration = true;
       enableNushellIntegration = true;
+      flavors = {
+        catppuccin-mocha = "${flavors}/flavors/catppuccin-mocha.yazi";
+        catppuccin-macchiato = "${flavors}/catppuccin-macchiato.yazi";
+      };
+      theme = {
+        flavor = {
+          use = "catppuccin-mocha";
+        };
+      };
+      plugins = {
+        glow = pkgs.fetchFromGitHub {
+          owner = "Reledia";
+          repo = "glow.yazi";
+          rev = "main";
+          hash = "sha256-celObHo9Y3pFCd1mTx1Lz77Tc22SJTleRblAkbH/RqY=";
+        };
+        rich-preview = pkgs.fetchFromGitHub {
+          owner = "AnirudhG07";
+          repo = "rich-preview.yazi";
+          rev = "main";
+          hash = "sha256-celObHo9Y3pFCd1mTx1Lz77Tc22SJTleRblAkbH/RqY=";
+        };
+      };
+      settings = {
+        manager = {
+          show_hidden = true;
+        };
+        plugin = {
+          prepend_previewers = [
+            # { name = "*.md"; run = "glow"; }
+            {
+              name = "*.csv";
+              run = "rich-preview";
+            } # for csv files
+            {
+              name = "*.md";
+              run = "rich-preview";
+            } # for markdown (.md) files
+            {
+              name = "*.rst";
+              run = "rich-preview";
+            } # for restructured text (.rst) files
+            {
+              name = "*.ipynb";
+              run = "rich-preview";
+            } # for jupyter notebooks (.ipynb)
+            {
+              name = "*.json";
+              run = "rich-preview";
+            } # for json (.json) files
+            #    { name = "*.lang_type"; run = "rich-preview"} # for particular language files eg. .py, .go., .lua, etc.
+          ];
+        };
+      };
     };
   };
 }
