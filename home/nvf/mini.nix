aef529a1c1b62ac9281833f8f09935d3681aa2ea
talarys <d.simon.ssx0@gmail.com>
2025-11-18 00:43:52 +0100

    Add additional settings and features to mini.nvim configuration

--- home/nvf/mini.nix
+++ home/nvf/mini.nix
@@ -12,5 +12,38 @@
     snippets.enable = true; # Manage and expand snippets
     splitjoin.enable = true; # Split and join arguments
     surround.enable = true; # Surround actions
+
+    # General workflow
+    basics.enable = true; # common configuration presets
+    bracketed.enable = true; # Go forward/backward with square brackets
+    bufremove.enable = true; # Remove buffers
+    clue.enable = true; # Show next key clues
+    deps.enable = true; # Plugin manager
+    diff.enable = true; # Work with diff hunks
+    extra.enable = true; # Extra 'mini.nvim' functionality
+    files.enable = true; # Navigate and manipulate file system
+    git.enable = true; # Git integration
+    jump.enable = true; # Jump to next/previous single character
+    jump2d.enable = true; # Jump within visible lines
+    misc.enable = true; # Miscellaneous functions
+    pick.enable = true; # Pick anything
+    sessions.enable = true; # Session management
+    visits.enable = true; # Track and reuse file system visits
+
+    # Appearance
+    animate.enable = true; # Animate common Neovim actions
+    base16.enable = true; # Base16 colorscheme creation
+    colors.enable = true; # Tweak and save any color scheme
+    cursorword.enable = true; # Autohighlight word under cursor
+    hipatterns.enable = true; # Highlight patterns in text
+    hues.enable = true; # Generate configurable color scheme
+    icons.enable = true; # Icon provider
+    indentscope.enable = true; # Visualize and work with indent scope
+    map.enable = true; # Window with buffer text overview
+    notify.enable = true; # Show notifications
+    starter.enable = true; # Start screen
+    statusline.enable = true; # Statusline
+    tabline.enable = true; # Tabline
+    trailspace.enable = true; # Trailspace (highlight and remove)
   };
 }
