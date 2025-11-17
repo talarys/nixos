78e0c3752fb13af79c03f026bafaf7a840b0955e
talarys <d.simon.ssx0@gmail.com>
2025-11-17 15:51:42 +0100

    Refactor default.nix to use a local variable for system platform

--- lib/default.nix
+++ lib/default.nix
@@ -1,14 +1,17 @@
-{ inputs }:
+{ inputs, ... }:
+let
+  system = inputs.pkgs.stdenv.hostPlatform.system;
+in
 rec {
   inherit (inputs) lib;
   inherit (import ./eachSystem.nix { inherit inputs; }) eachSystem;
   inherit (import ./treefmt.nix { inherit inputs eachSystem; }) treefmtEval;
   inherit (import ./mkHost.nix { inherit inputs; }) mkHost;
 
-  formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+  formatter = eachSystem (pkgs: treefmtEval.${system}.config.build.wrapper);
 
   checks = eachSystem (pkgs: {
-    formatting = treefmtEval.${pkgs.system}.config.build.check inputs.self;
+    formatting = treefmtEval.${system}.config.build.check inputs.self;
   });
 
   devShells = eachSystem (pkgs: {
