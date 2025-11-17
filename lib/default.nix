4b0316e9d3939f7129e9d60afb75b875f7f6850a
talarys <d.simon.ssx0@gmail.com>
2025-11-17 16:27:49 +0100

    Refactor default.nix to directly use pkgs.stdenv.hostPlatform.system for clarity

--- lib/default.nix
+++ lib/default.nix
@@ -1,23 +1,20 @@
 { inputs, ... }:
-let
-  system = inputs.pkgs.stdenv.hostPlatform.system;
-in
 rec {
   inherit (inputs) lib;
   inherit (import ./eachSystem.nix { inherit inputs; }) eachSystem;
   inherit (import ./treefmt.nix { inherit inputs eachSystem; }) treefmtEval;
   inherit (import ./mkHost.nix { inherit inputs; }) mkHost;
 
-  formatter = eachSystem (pkgs: treefmtEval.${system}.config.build.wrapper);
+  formatter = eachSystem (pkgs: treefmtEval.${pkgs.stdenv.hostPlatform.system}.config.build.wrapper);
 
   checks = eachSystem (pkgs: {
-    formatting = treefmtEval.${system}.config.build.check inputs.self;
+    formatting = treefmtEval.${pkgs.stdenv.hostPlatform.system}.config.build.check inputs.self;
   });
 
   devShells = eachSystem (pkgs: {
     default = import ./dev-shell.nix {
       inherit inputs;
-      inherit (pkgs) system;
+      inherit (pkgs.stdenv.hostPlatform.system) system;
     };
   });
 }
