f4953dea1a326c2bbba20993b72df4825cfc8826
talarys <d.simon.ssx0@gmail.com>
2025-11-17 16:12:26 +0100

    Reestructured /home

--- home/default.nix
+++ home/default.nix
@@ -1,15 +1,43 @@
 { lib, ... }:
 let
-  inherit (builtins) listToAttrs baseNameOf filter;
-  inherit (lib.filesystem) listFilesRecursive;
-  inherit (lib.strings) hasSuffix removeSuffix;
+  inherit (builtins)
+    listToAttrs
+    baseNameOf
+    filter
+    map
+    pathExists
+    ;
+
+  inherit (lib.filesystem)
+    listFilesRecursive
+    pathType
+    ;
+
+  inherit (lib.strings)
+    hasSuffix
+    removeSuffix
+    ;
+
+  fileModules = lib.pipe ./. [
+    listFilesRecursive
+    (filter (p: hasSuffix ".nix" p && p != ./default.nix))
+    (map (p: {
+      name = removeSuffix ".nix" (baseNameOf p);
+      value = p;
+    }))
+    listToAttrs
+  ];
+
+  dirModules = lib.pipe ./. [
+    listFilesRecursive
+    (filter (p: pathType p == "directory"))
+    (filter (dir: pathExists (dir + "/default.nix")))
+    (map (dir: {
+      name = baseNameOf dir;
+      value = dir;
+    }))
+    listToAttrs
+  ];
+
 in
-lib.pipe ./. [
-  listFilesRecursive
-  (filter (p: hasSuffix ".nix" p && p != ./default.nix))
-  (map (p: {
-    name = removeSuffix ".nix" (baseNameOf p);
-    value = p;
-  }))
-  listToAttrs
-]
+fileModules // dirModules
