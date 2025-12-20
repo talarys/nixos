7d673bac19ca96aa145ab9b3d862a55461def276
talarys <d.simon.ssx0@gmail.com>
2025-12-20 19:05:24 +0100

    Added quickshell config

--- modules/system/quickshell.nix
+++ modules/system/quickshell.nix
@@ -1,34 +1,46 @@
 {
   pkgs,
+  config,
+  lib,
   ...
 }:
+with lib;
+let
+  cfg = config.modules.system.quickshell;
+in
 {
-  environment.systemPackages = with pkgs; [
-    quickshell
-
-    # Qt6 related kits（for slove Qt5Compat problem）
-    qt6.qt5compat
-    qt6.qtbase
-    qt6.qtquick3d
-    qt6.qtwayland
-    qt6.qtdeclarative
-    qt6.qtsvg
-
-    # alternate options
-    # libsForQt5.qt5compat
-    kdePackages.qt5compat
-    libsForQt5.qt5.qtgraphicaleffects
-  ];
-
-  # necessary environment variables
-  environment.variables = {
-    QML_IMPORT_PATH = "${pkgs.qt6.qt5compat}/lib/qt-6/qml:${pkgs.qt6.qtbase}/lib/qt-6/qml";
-    QML2_IMPORT_PATH = "${pkgs.qt6.qt5compat}/lib/qt-6/qml:${pkgs.qt6.qtbase}/lib/qt-6/qml";
+  options.modules.system.quickshell = {
+    enable = mkEnableOption "Quickshell Configuration";
   };
 
-  # make sure the Qt application is working properly
-  environment.sessionVariables = {
-    QT_QPA_PLATFORM = "wayland;xcb";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      quickshell
+
+      # Qt6 related kits（for slove Qt5Compat problem）
+      qt6.qt5compat
+      qt6.qtbase
+      qt6.qtquick3d
+      qt6.qtwayland
+      qt6.qtdeclarative
+      qt6.qtsvg
+
+      # alternate options
+      # libsForQt5.qt5compat
+      kdePackages.qt5compat
+      libsForQt5.qt5.qtgraphicaleffects
+    ];
+
+    # necessary environment variables
+    environment.variables = {
+      QML_IMPORT_PATH = "${pkgs.qt6.qt5compat}/lib/qt-6/qml:${pkgs.qt6.qtbase}/lib/qt-6/qml";
+      QML2_IMPORT_PATH = "${pkgs.qt6.qt5compat}/lib/qt-6/qml:${pkgs.qt6.qtbase}/lib/qt-6/qml";
+    };
+
+    # make sure the Qt application is working properly
+    environment.sessionVariables = {
+      QT_QPA_PLATFORM = "wayland;xcb";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    };
   };
 }
